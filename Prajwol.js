3ee63933080628d34114258f94186e5a22cf326b
Prajwolkharel10 <prajwolkharel11@gmail.com>
2023-07-16 07:35:15 +0545

    let sum added

--- Prajwol.js
+++ Prajwol.js
@@ -1,9 +1,9 @@
 
-const arr =[1,2,3,4]
-
- arr.map(item=>{
-  if (item%2===0)
-sum=sum+item
+const arr = [1, 2, 3, 4]
+let sum = 0
+arr.map(item => {
+    if (item % 2 === 0)
+        sum = sum + item
 
 })
-
+console.log(sum)
